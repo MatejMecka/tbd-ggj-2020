6329733bf1de1bee50fe4c2fbbfca92bf118decf
matejmecka <matej.plavevski+github@gmail.com>
2020-02-01 15:55:51 +0100

    Added Menu, More Improvements to Sentences

---
+++ Assets/Scripts/HandlePlayers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandlePlayers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int currentPlayerId = 0;
+    public GameObject currentPlayer;
+
+    void Start()
+    {
+        // Create A Class for Each Player
+        int numPlayers =  PlayerPrefs.GetInt("NumberOfPlayers", 0);
+        print(numPlayers);
+        for(var i=0; i < numPlayers; i++){
+            GameObject player;
+            player = new GameObject("Player"  + i.ToString());
+            player.AddComponent<Player>();
+        }
+
+        currentPlayer = GameObject.Find("Player"  + currentPlayerId.ToString());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SentenceBehaviour.cs
+++ Assets/Scripts/SentenceBehaviour.cs
@@ -13,30 +13,41 @@ public class SentenceBehaviour : MonoBehaviour
     public Text sentence;
     public string fileName;
     private string[] lines = new string[]{};
+    public Player player;
+    private int level = 0;
+    private int cycles = 0;
+    private List<int> sentencesUsed = new List<int>();
 
     //public StreamReader reader = new StreamReader(path);
 
-    void Start()
-    {
+    void Start(){
         var sr = new StreamReader(Application.dataPath + "/" + fileName);
         var fileContents = sr.ReadToEnd();
         sr.Close();
 
         lines = fileContents.Split("\n"[0]);
-        getNewSentence();
+        getNewSentence(level+3);
     }   
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
        // print("test");
     }
 
-    void getNewSentence(){
+    void getNewSentence(int round){
         Sentence = lines[Random.Range(0, lines.Length)];
-        //print("RANDOM: " + lines[Random.Range(0, lines.Length)]);
         splitSentence(Sentence);
 
+        // Check if Sentence equals
+        if(keywords.Length != round){
+            getNewSentence(round);
+        }
+
+        for(int i=0; i < keywordsShuffled.Count; i++){
+             keywordsShuffled[i] = null;
+        }
+
+        // Add Keywords
         for(int i=0; i < keywords.Length; i++){
             keywordsShuffled.Add(keywords[i]);
         }
@@ -56,7 +67,6 @@ public class SentenceBehaviour : MonoBehaviour
     }
 
     void shuffleKeywords(){
-
         // Shuffle Words to show to User
         for(int i=0; i < keywordsShuffled.Count; i++){
             int rnd = Random.Range(0, keywordsShuffled.Count);
@@ -66,23 +76,41 @@ public class SentenceBehaviour : MonoBehaviour
         }
         
         // Verify it is Randomized
+        for(int i=0; i < keywords.Length; i++){
+            if(keywords[i]==keywordsShuffled[i]){
+                shuffleKeywords();
+                break;
+            }
+        }
+
+        /* 
         if(keywords == keywordsShuffled.ToArray()){
             for(int i=0; i < keywordsShuffled.Count; i++){
                 keywordsShuffled[i] = null;
             }
             shuffleKeywords();
-        }
+        }*/
     }
 
     void validateSentence(){
         ;
         /*
+            GameObject player = HandlePlayers.currentPlayer
+            cycles++
             if(keywords.Length == guesses.Count &6 keywords == guesses.Count){
                 // Increase Scores
-                score++;
-                time = ""
+               // player.score++;
+                player.corrects++;
+            } else {
+                player.wrongs++;
             }
-            getNewSentence();
+            if(cycles == 5){
+                level++;
+                cycles = 0;
+            }
+            HandlePlayers.currentPlayerId++;
+            GameObject.currentPlayer = GameObject.Find("Player"  + currentPlayerId.ToString());
+            getNewSentence(level);
          */
     }
 
